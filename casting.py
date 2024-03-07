5f17d8a53f300b81e034afda583359a880073475
sinan.yilmaz <sinan.yilmaz@itoworld.com>
2024-03-07 09:43:33 +0000

    casting variable

---
+++ casting.py
@@ -0,0 +1,11 @@
+x = str(3.14)  # x = "3.14"
+x2 = 3.14
+x3 = "3.14"
+y = int(3.14)  # y = 3
+z = float(3)  # z = 3.0
+
+print(x)  # string "3.14"
+print(x2)  # float 3.14
+print(x3)  # string "3.14"
+print(y)
+print(z)
